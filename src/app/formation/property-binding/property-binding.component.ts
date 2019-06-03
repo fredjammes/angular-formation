41a4f8777bbc875a32ea379489516fcf2863a360
Frédéric JAMMES <fjammes@sqli.com>
2019-06-03 16:01:02 +0200

    Ajout d'exemple sur disabled dans property-binding

--- src/app/formation/property-binding/property-binding.component.ts
+++ src/app/formation/property-binding/property-binding.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class PropertyBindingComponent {
   inputPlaceholder = 'Type an awesome text here';
+  isDisabled = true;
   isSquareRed = false;
   isSquare = false;
 }
