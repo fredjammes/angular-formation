38a39fd9e562dbdc36d463147b2292a5223dab55
Frédéric JAMMES <fjammes@sqli.com>
2019-06-03 10:44:11 +0200

    Ajout d'un feat module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyFeatModule } from './my-feat/my-feat.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyFeatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
