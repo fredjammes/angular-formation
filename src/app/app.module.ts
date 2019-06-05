f073d7db4b9def5e03b700775eccd8a08545b5ac
Frédéric JAMMES <fjammes@sqli.com>
2019-06-05 07:54:41 +0200

    ajout du composant d'intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import {AppComponent} from './app.component';
 import {FormationModule} from './formation/formation.module';
 import {AppRoutingModule} from './app-routing.module';
 import {RouterModule} from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
