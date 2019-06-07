5cbd2487c17ce28706d71e6fd84f5111245620bb
Frédéric JAMMES <fjammes@sqli.com>
2019-06-07 16:24:07 +0200

    (InputResizer8 - Service) Look & feel - ajout de material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HighlightDirective } from './highlight.directive';
 import { MonPipePipe } from './mon-pipe.pipe';
 import { SizeLabelPipe } from './size-label.pipe';
-import {HttpClientModule} from '@angular/common/http';
 import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule} from '@angular/material';
 
 @NgModule({
